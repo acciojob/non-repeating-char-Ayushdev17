76ba24f5f85554d5e3bf8a6fd5eb574f75b0fb6b
Ayushdev17 <devayush176@gmail.com>
2025-02-10 12:33:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 // Create a frequency map to store the frequency of each character
+    let frequencyMap = {};
+
+    // Populate the frequency map
+    for (let char of str) {
+        frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+    }
+
+    // Find the first non-repeated character
+    for (let char of str) {
+        if (frequencyMap[char] === 1) {
+            return char;
+        }
+    }
+
+    // If no non-repeated character is found, return null
+    return null;
+ 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
